f508ec110097ad2bcef7fed6c7970edca3e4fd5d
SeungYoon Kim <dskym@naver.com>
2019-10-13 23:52:58 +0900

    update

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,11 +16,6 @@ Including another URLconf
 from django.urls import include
 from django.conf.urls import url
 
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='mondey')
-
 urlpatterns = [
-    url(r'^$', schema_view, name='swagger'),
     url(r'', include('mondey.urls'), name='mondey'),
 ]
